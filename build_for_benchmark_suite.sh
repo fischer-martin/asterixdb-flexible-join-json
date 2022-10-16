34d061564cfbf2fa57aa090e82ea0fee3b394120
Martin Fischer <martin.fischer@stud.sbg.ac.at>
2022-10-16 13:53:58 +0200

    fix path in build_for_benchmark_suize.sh

--- build_for_benchmark_suite.sh
+++ build_for_benchmark_suite.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-rm -f flexiblejoin.jar.zip && mvn package && zip -j flexiblejoin.jar.zip target/*.jar && cp flexiblejoin.jar.zip ../asterixdb-benchmarking/lib/flexiblejoin.jar.zip
+rm -f flexiblejoin.jar.zip && mvn package && zip -j flexiblejoin.jar.zip target/*.jar && cp flexiblejoin.jar.zip ../asterixdb-benchmarking/data/lib/flexiblejoin.jar.zip
